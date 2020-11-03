f8eb99c040d1b18a41242f5cce53bd7ddd4650b3
MaelDrelon <mael.drelon1@gmail.com>
2020-11-03 16:44:01 +0100

    upd

--- Partie 2/Exo1.php
+++ Partie 2/Exo1.php
@@ -1,23 +1,23 @@
 <?php 
     function tableau()
     {
-        echo "<table><td>Exemple</td></table>";
+        echo "<table>
+                        <td>
+                            Exemple
+                        </td>
+              </table>";
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CssExo1.css"> 
 </head>
 <body>
     <div>
         <?php tableau(); ?>
     </div>
 
-    <div class="basdepag">
+    <div class=bas>
         <?php tableau(); ?>
     </div>
 </body>
