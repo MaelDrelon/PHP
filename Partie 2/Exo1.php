731f280da8a7bdd464bd5dca80cecc02a5f334ce
MaelDrelon <mael.drelon1@gmail.com>
2020-11-03 16:27:51 +0100

    upd

---
+++ Partie 2/Exo1.php
@@ -0,0 +1,23 @@
+<?php 
+    function tableau()
+    {
+        echo "<table><td>Exemple</td></table>";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    <div>
+        <?php tableau(); ?>
+    </div>
+
+    <div class="basdepag">
+        <?php tableau(); ?>
+    </div>
+</body>
