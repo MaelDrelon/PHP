312db125e821710a1f153c406470a2ecb785a6c6
MaelDrelon <mael.drelon1@gmail.com>
2020-11-05 10:26:16 +0100

    upd

---
+++ Partie 2/Exo2.php
@@ -0,0 +1,26 @@
+<?php 
+    $tableaux1="Crash";
+    $tableaux2="Spyro";
+    $tableaux3="Bundle";
+    function tableau()
+    {
+        echo "<table>
+                        <td>
+                            
+                        </td>
+                        <td>
+
+                        </td>
+                        <td>
+
+                        </td>
+              </table>";
+    }
+?>
+
+<head>
+    <link rel="stylesheet" href="CssExo1.css"> 
+</head>
+<body>
+        <?php tableau(); ?>
+</body>
